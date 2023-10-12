db8239c31b3fe593ea6628ef6c2f2631e0b2fde8
noe <noe.voneiff@gmail.com>
2023-10-12 22:40:37 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let input: [u8; 81] = [
+        8, 5, 0, 0, 0, 2, 4, 0, 0,
+        7, 2, 0, 0, 0, 0, 0, 0, 9,
+        0, 0, 4, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 7, 0, 0, 2, 3,
+        0, 5, 0, 0, 0, 9, 0, 0, 0,
+        4, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 8, 0,
+        0, 0, 7, 0, 0, 1, 7, 0, 0,
+        0, 0, 0, 3, 6, 0, 4, 0, 0,
+    ];
+
+    draw_sudoku(&input);
+}
+
+fn draw_sudoku(board: &[u8; 81]) {
+    println!("+-----------------------------+");
+    for (i, &val) in board.iter().enumerate() {
+        let line_start = if i % 9 == 0 { "|" } else { "" };
+        let cell_value = if val == 0 { String::from(" ") } else { val.to_string() };
+        let cell_separator = if i % 3 == 2 { "|" } else { "" };
+        let line_end = if i % 9 == 8 { "\n" } else { "" };
+        
+        print!("{} {} {}{}", line_start, cell_value, cell_separator, line_end);
+        
+        if i % 27 == 26 && i < 80 {
+            println!("|---------+---------+---------|");
+        }// else if i % 9 == 8 && i < 80 {
+        //     println!("|         |         |         |");
+        // }
+    }
+    println!("+-----------------------------+");
+}
