dc1fad38ed6c6e5207da20d9dc797e77769057e0
Noe <noe.voneiff@gmail.com>
2023-11-03 01:30:17 +0100

    Changed board type from [u8; 81] to [[u8; 9]; 9]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::collections::HashSet;
+use std::time::{Duration, Instant};
 
 struct Sudoku {
     // Flat list of the sudoku board.
-    board: [u8; 81],
+    board: [[u8; 9]; 9],
     // Create a blacklist of every number that can't be placed on its specific index.
     // This is a 2D Array where for eg. the first array is the list of numbers that can't be
     // placed in the first cell of the input.
@@ -10,16 +11,21 @@ struct Sudoku {
 }
 
 fn main() {
-    let mut sudoku = Sudoku::new_easy();
+    let mut sudoku: Sudoku = Sudoku::new_easy();
 
     sudoku.draw();
 
-    let correct = sudoku.is_solved();
+    let correct: bool = sudoku.is_solved();
     println!("{}", correct.to_string());
 
+    let start: Instant = Instant::now();
     sudoku.solve();
+    let duration: Duration = start.elapsed();
+    println!("Time elapsed to solve sudoku: {:?}", duration);
 
-    let correct = sudoku.is_solved();
+    sudoku.draw();
+
+    let correct: bool = sudoku.is_solved();
     println!("{}", correct.to_string());
 }
 
@@ -27,22 +33,32 @@ impl Sudoku {
     fn solve(&mut self) {
         // Solve the Sudoku (the crux!)
         self.blacklist[0][2] = 12;
-        dbg!(self.blacklist);
+        //dbg!(self.blacklist);
+
+        while !self.is_solved() {
+            // 1. Perform several checks to add entries to the blacklist
+            // for cell in board
+            // vertical lines check
+            // horizontal lines check
+
+            // 2. Fill cells of the board where the blacklist is only missing one number
+            break;
+        }
     }
 
     fn new_easy() -> Self {
         // TEMPORARY: Returns an easy to solve Sudoku
         Self {
             board: [
-                0, 3, 4, 6, 7, 8, 9, 1, 2, // 5
-                6, 7, 2, 1, 9, 5, 3, 4, 8,
-                1, 9, 8, 3, 4, 2, 5, 6, 7,
-                8, 5, 9, 7, 6, 1, 4, 2, 3,
-                4, 2, 6, 8, 5, 3, 7, 9, 1,
-                7, 1, 3, 9, 2, 4, 0, 5, 6, // 8
-                9, 6, 1, 5, 3, 7, 2, 8, 4,
-                2, 8, 7, 4, 1, 9, 6, 3, 5,
-                3, 4, 5, 2, 8, 0, 1, 7, 9, // 6
+                [0, 3, 4, 6, 7, 8, 9, 1, 2], // 5
+                [6, 7, 2, 1, 9, 5, 3, 4, 8],
+                [1, 9, 8, 3, 4, 2, 5, 6, 7],
+                [8, 5, 9, 7, 6, 1, 4, 2, 3],
+                [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                [7, 1, 3, 9, 2, 4, 0, 5, 6], // 8
+                [9, 6, 1, 5, 3, 7, 2, 8, 4],
+                [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                [3, 4, 5, 2, 8, 0, 1, 7, 9], // 6
             ],
             blacklist: [[0u8; 9]; 81],
         }
@@ -52,15 +68,15 @@ impl Sudoku {
         // TEMPORARY: Returns a hard to solve Sudoku
         Self {
             board: [
-                8, 5, 0, 0, 0, 2, 4, 0, 0,
-                7, 2, 0, 0, 0, 0, 0, 0, 9,
-                0, 0, 4, 0, 0, 0, 0, 0, 0,
-                0, 0, 1, 0, 7, 0, 0, 2, 3,
-                0, 5, 0, 0, 0, 9, 0, 0, 0,
-                4, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 8, 0,
-                0, 0, 7, 0, 0, 1, 7, 0, 0,
-                0, 0, 0, 3, 6, 0, 4, 0, 0,
+                [8, 5, 0, 0, 0, 2, 4, 0, 0],
+                [7, 2, 0, 0, 0, 0, 0, 0, 9],
+                [0, 0, 4, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 7, 0, 0, 2, 3],
+                [0, 5, 0, 0, 0, 9, 0, 0, 0],
+                [4, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 8, 0],
+                [0, 0, 7, 0, 0, 1, 7, 0, 0],
+                [0, 0, 0, 3, 6, 0, 4, 0, 0],
             ],
             blacklist: [[0u8; 9]; 81],
         }
@@ -69,11 +85,12 @@ impl Sudoku {
     fn draw(&self) {
         // Print the Sudoku in a readable manner
         println!("+-----------------------------+");
-        for (i, &val) in self.board.iter().enumerate() {
-            let cell_separator_left = if i % 9 == 0 { "|" } else { "" };
-            let cell_value = if val == 0 { String::from(" ") } else { val.to_string() };
-            let cell_separator_right = if i % 3 == 2 { "|" } else { "" };
-            let line_end = if i % 9 == 8 { "\n" } else { "" };
+        for i in 0..81 {
+            let val: u8 = self.board[i / 9][i % 9];
+            let cell_separator_left: &str = if i % 9 == 0 { "|" } else { "" };
+            let cell_value: String = if val == 0 { String::from(" ") } else { val.to_string() };
+            let cell_separator_right: &str = if i % 3 == 2 { "|" } else { "" };
+            let line_end: &str = if i % 9 == 8 { "\n" } else { "" };
             
             print!("{} {} {}{}", cell_separator_left, cell_value, cell_separator_right, line_end);
             
@@ -89,14 +106,15 @@ impl Sudoku {
     fn is_solved(&self) -> bool {
         // Return true if the sudoku is solved, false if not
         // Empty cells
-        if self.board.contains(&0) {
-            return false;
+        for row in self.board {
+            if row.contains(&0) {
+                return false;
+            }
         }
 
         // Test rows
-        for i in 0..9 { // Row index
-            let row: &[u8] = &self.board[9 * i .. 9 * (i + 1)];
-            if has_duplicates(row) {
+        for row in self.board {
+            if has_duplicates(&row) {
                 return false;
             }
         }
@@ -105,7 +123,7 @@ impl Sudoku {
         for i in 0..9 { // Column index
             let mut column: [u8; 9] = [0; 9];
             for j in 0..9 { // Vertical index
-                column[j] = self.board[j * 9 + i];
+                column[j] = self.board[j][i];
             }
 
             if has_duplicates(&column) {
@@ -135,13 +153,14 @@ impl Sudoku {
     fn load(path: String) -> Self {
         // Load a sudoku from a file and return a Sudoku struct
         Self {
-            board: [0u8; 81],
+            board: [[0u8; 9]; 9],
             blacklist: [[0u8; 9]; 81],
         }
     }
 }
 
 fn has_duplicates(arr: &[u8]) -> bool {
+    // TODO Move to a util file
     let mut set: HashSet<u8> = HashSet::new();
 
     for &element in arr {
